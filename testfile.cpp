303e1a81a01ac8f3c2bb8749f4fa8406d73cf507
aaaaa <sdgsg>
2024-10-22 21:17:00 +0300

    testfile file addded

---
+++ testfile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+//#include <string>
+
+int main(){
+	std::vector <int> v(10);
+	//v[100000] = 1;
+	int x = 1;
+	x /= v[0];
+	//std::cout << "Hello, world!" << '\n';
+	//f
+	return 0;
+}
